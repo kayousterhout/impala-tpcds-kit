24eb3a08d99ed82771c05dd382c2c26168514171
Kay Ousterhout <kayousterhout@gmail.com>
2014-04-21 17:56:00 +0000

    Use smaller scale factor and auto-set nodes/threads

--- tpcds-env.sh
+++ tpcds-env.sh
@@ -6,13 +6,14 @@ export FLATFILE_HDFS_ROOT=/user/${USER}/tpcds
 
 # scale factor in GB
 # SF 3000 yields ~1TB for the store_sales table
-export TPCDS_SCALE_FACTOR=3000
+export TPCDS_SCALE_FACTOR=100
 
 # this is used to determine the number of dsdgen processes to generate data
 # usually set to one per physical CPU core
 # example - 20 nodes @ 12 threads each
-export DSDGEN_NODES=20
-export DSDGEN_THREADS_PER_NODE=12
+export DSDGEN_NODES=`grep -c ^ /root/spark-ec2/slaves`
+# Assumes that the master is the same machine type as the slaves.
+export DSDGEN_THREADS_PER_NODE=`grep -c ^processor /proc/cpuinfo`
 export DSDGEN_TOTAL_THREADS=$((DSDGEN_NODES * DSDGEN_THREADS_PER_NODE))
 
 # the name for the tpcds database
